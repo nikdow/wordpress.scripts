b1e1bf9452b882256908ddd154871b6c7774fe2b
aws02 user <nik@cbdweb.net>
2025-07-07 19:15:28 +1000

    first commit

---
+++ update_items.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+import os
+import requests
+from time import sleep
+import zipfile
+from packaging.version import Version
+from abc import ABC, abstractmethod
+from bs4 import BeautifulSoup
+
+
+PLUGIN_DIR = "/home/lamp/wordpress/plugins"
+THEME_DIR = "/home/lamp/wordpress/themes"
+
+# List premium plugins to exclude
+EXCLUDED_PLUGINS = [
+    "newsletter-comments",
+    "newsletter-import",
+    "thim-core",
+    "pmpro-pay-by-check",
+    "paid-memberships-pro",
+    "newsletter-automated",
+    "newsletter-facebook",
+    "newsletter-woocommerce",
+    "newsletter-archive",
+    "wp-mail-smtp-pro",
+    "newsletter-amazon",
+    "pmpro-akismet",
+    "newsletter-extensions",
+    "pmpro-add-member-admin",
+    "newsletter-reports",
+    "jetpack",
+]
+
+# List premium themes to exclude
+EXCLUDED_THEMES = [
+    "Avada",
+    "Divi",
+    "ascend",
+    "documentation-suburbia-child",
+    "jolene",
+    "sailing3",
+    "sailing3.old",
+    "salient",
+    "salient10.5",
+    "simpleblock",
+    "spacious-pro",
+    "spacious.1.4.7",
+    "spacious.1.6.6",
+    "suburbia",
+    "supernews",
+    "twentytwentytwo.bak",
+]
+
+
+class Item(ABC):
+    @abstractmethod
+    def get_latest_item(self, name):
+        """Fetch the latest version information."""
+
+    def update_item(self, item, current_version, excluded_items, directory):
+        """Common update logic for plugins and themes."""
+        if item in excluded_items:
+            print(f"⏭ Skipping excluded item: {item}")
+            return
+
+        latest_info = self.get_latest_item(item)
+        if not latest_info:
+            return
+
+        sleep(4)
+
+        try:
+            latest_version = latest_info["version"]
+            download_url = latest_info["download_link"]
+            if (
+                current_version == "trunk"
+                or latest_version == "trunk"
+                or Version(latest_version) > Version(current_version)
+            ) and download_url:
+                print(
+                    f"⬆ Updating {item}: {current_version} → {latest_version}"
+                )
+                sleep(3)
+                self.download_and_extract(download_url, directory, item)
+            else:
+                print(f"⛔ {item} is up to date ({current_version})")
+        except Exception:
+            print(f"⚠️  Invalid version for {item}: {latest_version}")
+
+    def update(self, directory, excluded_items):
+        """Update items by checking their versions and downloading updates."""
+        installed_items = self.get_installed_versions(directory)
+        for item, current_version in installed_items.items():
+            self.update_item(item, current_version, excluded_items, directory)
+
+    def get_readme_file_path(self, plugin_path):
+        """Get the path to the readme file."""
+        readme_files = ("readme.txt", "README.txt")
+
+        if os.path.isdir(plugin_path):
+            for readme_file in readme_files:
+                file_path = os.path.join(plugin_path, readme_file)
+                if os.path.isfile(file_path):
+                    return file_path
+        return False
+
+    def get_installed_versions(self, directory):
+        """Retrieve the currently installed plugins or themes and their versions."""
+        items = {}
+        for item in os.listdir(directory):
+            git_path = os.path.join(directory, item, ".git")
+            if os.path.exists(git_path):
+                continue
+
+            plugin_path = os.path.join(directory, item)
+            readme_path = self.get_readme_file_path(plugin_path)
+            if readme_path:
+                version = "unknown"
+                with open(
+                    readme_path, "r", encoding="utf-8", errors="ignore"
+                ) as f:
+                    for line in f:
+                        if "Stable tag:" in line:
+                            version = line.split(":")[1].strip()
+                            break
+                items[item] = version
+
+        sorted_versions = {k: items[k] for k in sorted(items)}
+        return sorted_versions
+
+    def download_and_extract(self, zip_url, extract_to, plugin):
+        """Download and extract a ZIP file to the specified directory."""
+        zip_path = "/tmp/temp.zip"
+        response = requests.get(zip_url, stream=True)
+        if response.status_code == 200:
+            with open(zip_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    f.write(chunk)
+            with zipfile.ZipFile(zip_path, "r") as zip_ref:
+                zip_ref.extractall(extract_to)
+            os.remove(zip_path)
+            print(f"✅ Updated: {os.path.join(extract_to, plugin)}")
+        else:
+            print(f"❌ Failed to download: {zip_url}")
+
+
+class Plugin(Item):
+    def get_latest_item(self, name):
+        """Fetch the latest version information from the WordPress API."""
+        api_url = f"https://api.wordpress.org/plugins/info/1.0/{name}.json"
+
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            return response.json()
+
+        print(
+            f"⚠️  API request failed for {name}: {response.status_code} {response.reason}"
+        )
+
+        print(response.text)
+        return None
+
+
+class Theme(Item):
+    def get_latest_item(self, name):
+        """Fetch the latest version information."""
+        headers = {"User-Agent": "Mozilla/5.0"}
+        repo_url = f"https://wordpress.org/themes/{name}"
+        try:
+            response = requests.get(repo_url, headers=headers)
+            soup = BeautifulSoup(response.text, "html.parser")
+            download_link = soup.select_one(
+                "#wporg-theme-button-download"
+            ).attrs["href"]
+            version = soup.select_one(
+                ".is-meta-version span:nth-child(2)"
+            ).text
+
+            return {"version": version, "download_link": download_link}
+        except:
+            print(
+                f"⚠️  API request failed for {name}: {response.status_code} {response.reason}"
+            )
+
+        return None
+
+
+class App:
+    ITEM_CLASSES = {"plugin": Plugin, "theme": Theme}
+
+    @classmethod
+    def execute(cls, item, directory, excluded_items):
+        if item not in cls.ITEM_CLASSES:
+            raise ValueError(
+                f"Invalid item: '{item}'. Valid options are {', '.join(cls.ITEM_CLASSES.keys())}."
+            )
+
+        item_obj = getattr(cls.ITEM_CLASSES[item](), "update")
+        return item_obj(directory, excluded_items)
+
+
+if __name__ == "__main__":
+    print("🔄 Updating plugins...")
+    App.execute("plugin", PLUGIN_DIR, EXCLUDED_PLUGINS)
+
+    print("🔄 Updating themes...")
+    App.execute("theme", THEME_DIR, EXCLUDED_THEMES)
---
+++ upgrade_minor.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+#
+# Upgrade WP core to the latest minor version
+#
+# backup changes to this file by copying it into https://doc.cbdweb.net/documentation/technical/word-press/combined-wp-core/
+
+import re
+from bs4 import BeautifulSoup
+import http.client
+from packaging.version import Version
+import subprocess
+import sys
+import os
+
+
+def run_cmd(cmd):
+    output = subprocess.getoutput(cmd)
+    status = subprocess.getstatusoutput(cmd)[0]
+    if 0 != status:
+        print("  > cmd %s failed with status code %d" % (cmd, status))
+        sys.exit(-1)
+    return output
+
+
+def getCurrentVersion(dir):
+    file = os.path.join(dir, "wp-includes/version.php")
+    with open(file, encoding="utf-8") as f:
+        versions = re.findall("\$wp_version = '([\d\.]+)'", f.read())
+        if len(versions) > 0:
+            return versions[0]
+
+
+def getLatestTags():
+    conn = http.client.HTTPSConnection("core.svn.wordpress.org", 443)
+    conn.request("GET", "/tags/")
+    r1 = conn.getresponse()
+    html_doc = r1.read().decode("utf-8")
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    return [a.get('href').replace('/', '') for a in soup.find_all('a')]
+
+
+def getLatestReleases():
+    releases = []
+    conn = http.client.HTTPSConnection("wordpress.org", 443)
+    conn.request("GET", "/download/releases/")
+    html_doc = conn.getresponse().read().decode("utf-8")
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    for table in soup.find_all('div', id='latest'):
+        table = table.find('tbody')
+        for tr in table.find_all('tr'):
+            if tr is None:
+                continue
+            tds = tr.find_all(recursive=False)
+            releases.append((tds[0].get_text(),
+                             tds[1].get_text(),
+                             tds[2].find('a').get('href') if tds[2].find('a') is not None else '',
+                             tds[3].find('a').get('href') if tds[3].find('a') is not None else ''))
+    releases.reverse()
+    return releases
+
+
+def toMajorVersion(curVersion):
+    v = curVersion.split(".")
+    majorVersion = v[0] + "." + v[1]
+    return majorVersion
+
+
+def getLatestMinorRelease(curVersion):
+    for tag in getLatestReleases():
+        m = re.match(b'^\d+[\d.]*', tag[0].encode("utf-8"))
+        if m is None:
+            continue
+        if Version(tag[0]) > Version(curVersion):
+            return tag
+        else:
+            print("Latest version installed")
+            return
+    else:
+        print("No latest release found.")
+
+
+def upgradeWpDir(dir):
+    print("Checking for updating core %s ..." % dir)
+    curVersion = getCurrentVersion(dir)
+    if not curVersion:
+        print("Failed to extract WP version !!!")
+        return
+    print("Current WP version: ", curVersion)
+
+    release = getLatestMinorRelease(curVersion)
+    if release is None:
+        print("No update needed")
+        return 0
+
+    print('Latest Release found:', release)
+
+    minorUpdate = False
+    majorVersion = toMajorVersion(release[0])
+    if curVersion.find(majorVersion) == 0:
+        print('%s is a minor update ...' % release[0])
+        minorUpdate = True
+    elif os.path.exists(os.path.join(dir, '..', 'wp' + majorVersion)):
+        print('%s is a major update which is already locally available, going to skip it.' % release[0])
+        return
+
+    print('Downloading release %s ...' % release[3])
+    subprocess.getstatusoutput("rm -rf release.tar.gz")
+    download_url = release[3]
+    wget_command = ["wget", "-O", "release.tar.gz", download_url]
+    print('Download command:', ' '.join(wget_command))  # Print the wget command for debugging
+
+    try:
+        subprocess.run(wget_command, check=True)
+    except subprocess.CalledProcessError as e:
+        print('Error: failed to download.')
+        print('wget output:', e.output.decode('utf-8'))
+        exit(1)
+
+    print('Extracting ... ')
+    status, output = subprocess.getstatusoutput("tar -xzf release.tar.gz")
+    if status:
+        print('Error: failed to extract ...')
+        print(output)
+        exit(1)
+
+    target = "wordpress"
+
+    print("copy .htaccess ...")
+    status, output = subprocess.getstatusoutput("cp %s/.htaccess %s/.htaccess" % (dir, target))
+    if status:
+        print('Failed to copy .htaccess')
+        print(output)
+        exit(1)
+
+    print('Copying wp-config ...')
+    status, output = subprocess.getstatusoutput("cp %s/wp-config.php %s/wp-config.php" % (dir, target))
+    if status:
+        print('Failed to copy wp-config')
+        print(output)
+        exit(1)
+
+    print('Copying robots.txt ...')
+    status, output = subprocess.getstatusoutput("cp %s/robots.txt %s/robots.txt" % (dir, target))
+    if status:
+        print('Failed to copy: robots.txt')
+        print(output)
+        exit(1)
+
+    print('Chown ...')
+    status, output = subprocess.getstatusoutput("chown -R root:root %s" % target)
+    if status:
+        print('Failed to chown')
+        print(output)
+        exit(1)
+
+    print('rm wp-content ...')
+    status, output = subprocess.getstatusoutput("rm -R %s/wp-content/" % target)
+    if status:
+        print('Failed to rm wp-content')
+        exit(1)
+
+    print('ln ../wp-content ...')
+    status, output = subprocess.getstatusoutput("ln -s ../../wp-content/ %s/wp-content" % target)
+    if status:
+        print('Failed to ln ../wp-content')
+        print(output)
+        exit(1)
+
+    print('Backup tar ...')
+    status, output = subprocess.getstatusoutput("tar -zcvf %s-2020-05-02.tar.gz %s" % (dir, dir))
+    if status:
+        print('Failed to tar -xvf ...')
+        print(output)
+        return
+
+    # only remove in case of minorUpdate
+    if minorUpdate:
+        print('Remove dir %s ...' % dir)
+        status, output = subprocess.getstatusoutput("rm -rf %s" % dir)
+        if status:
+            print('Failed to remove dir')
+            print(output)
+            return
+
+    cmd = "mv wordpress wp" + toMajorVersion(release[0])
+    print(cmd)
+    status, output = subprocess.getstatusoutput(cmd)
+    if status:
+        print('Error: failed to mv to dir wp', toMajorVersion(release[0]))
+        print(output)
+        exit(1)
+
+    subprocess.getstatusoutput("rm -rf *.tar.gz")
+
+    cmd = "service php8.2-fpm reload"
+    print(cmd)
+    subprocess.getstatusoutput(cmd)
+
+
+coreDir = "/home/lamp/wordpress/core"
+os.chdir(coreDir)
+print("Working Dir: ", os.getcwd())
+
+for dir in os.listdir(coreDir):
+    dirPath = os.path.join(coreDir, dir)
+    if dir.find('wp') == 0 and os.path.isdir(dirPath):
+        upgradeWpDir(dirPath)
+        print('-----------------------------------')
