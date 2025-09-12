849a96850492563c26b850d2d74c241bc14966b8
nikdow <nik@nikdow.net>
2025-09-12 19:00:50 +1000

    rewrite jail.d/*.local file;

--- wpxmlrpc.py
+++ wpxmlrpc.py
@@ -20,17 +20,22 @@ def update_cloudfront_whitelist(json, jail_name):
 def update_fail2ban_whitelist(json, jail_name):
     response = requests.get(json)
     data = response.json()
-
+    with open('/etc/fail2ban/jail.d/' + jail_name + '.local', 'r') as f:
+        lines = f.readlines()
+    ignoreip = 'ignoreip = '
     for ip in data:
-             try:
-                 subprocess.run(['fail2ban-client', 'set', jail_name, 'addignoreip', ip], check=True)
-             except subprocess.CalledProcessError as e:
-                 print(f"Error whitelisting {ip}: {e}")
+        ignoreip += ip
+    with open('/etc/fail2ban/jail.d/' + jail_name + '.local', 'w') as f:
+        for line in lines:
+            if line.startswith('ignoreip'):
+                f.write(ignoreip)
+            else:
+                f.write(line)
 # params and invoke
 json_url = 'https://jetpack.com/ips-v4.json'
 fail2ban_jail = 'wpxmlrpc'  # Replace with your jail name
 update_fail2ban_whitelist(json_url, fail2ban_jail)
-json_url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
-fail2ban_jail = 'cloudfront'
-update_cloudfront_whitelist(json_url, fail2ban_jail)
+#json_url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+#fail2ban_jail = 'cloudfront'
+#update_cloudfront_whitelist(json_url, fail2ban_jail)
 subprocess.run(['fail2ban-client', 'reload' ])
