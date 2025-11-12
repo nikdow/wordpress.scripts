3572015b5e47c340bac7bbe9ecba5af5c7858679
nikdow <nik@nikdow.net>
2025-11-12 13:28:10 +1100

    log format altered to include response time (seconds);

--- apache_log.py
+++ apache_log.py
@@ -3,13 +3,13 @@ import re
 import sys
 import csv
 
-pattern = r'([a-zA-z0-9\-_\.]+)\:(\d+)\s([\d\.]+)\s(\d+)\s\[(\d+)\/([A-zA-z]{3})\/(\d{4}):(\d{2}):(\d{2}):(\d{2})\s[\+]?[\d]+\]\s"(GET|POST|PUT)\s([^\s]+)\sHTTP\/([\d\.]+)"\s(\d+)\s(\d+)\s"([^"]+)"\s"([^"]+)"'
+pattern = r'([a-zA-z0-9\-_\.]+)\:(\d+)\s([\d\.]+)\s(\d+)\s\[(\d+)\/([A-zA-z]{3})\/(\d{4}):(\d{2}):(\d{2}):(\d{2})\s[\+]?[\d]+\]\s"(GET|POST|PUT)\s([^\s]+)\sHTTP\/([\d\.]+)"\s(\d+)\s(\d+)\s(\d+)\s"([^"]+)"\s"([^"]+)"'
 
 def read_file(filename):
     count = 0
     with open(filename+'.csv', 'w', newline='', encoding='UTF8') as f_out:
         writer = csv.writer(f_out)
-        writer.writerow(['server_name', 'port', 'ip', 'apache process', 'day', 'month', 'year', 'hour', 'minute', 'second', 'request', 'path', 'protocol', 'status', 'size', 'referrer', 'user agent'])
+        writer.writerow(['server_name', 'port', 'ip', 'apache process', 'day', 'month', 'year', 'hour', 'minute', 'second', 'request', 'path', 'protocol', 'status', 'size', 'response (seconds)', 'referrer', 'user agent'])
         with open(filename, 'r') as f:
             for content in f:
                 match = re.match(pattern, content)
